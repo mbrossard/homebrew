6e8c0385091572cfd1c111dfdeb004b6a031e978
Mathias Brossard <mathias@brossard.org>
2016-09-04 00:02:49 -0700

    Creating a formula for pkcs11-util

--- pkcs11-util.rb
+++ pkcs11-util.rb
@@ -5,17 +5,12 @@ class Pkcs11Util < Formula
   version "1.0"
   sha256 "64bf761b8c854f086087eb40c2e2cad08016ba16bb2d7110845b14c4e5e77ec9"
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on :nss
-  depends_on :openssl
-
   def install
+    system "autoreconf -i"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
-                          "--prefix=#{prefix}",
-                          "--with-openssl=#{Formula["openssl"].opt_prefix}"
+                          "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
     system "make", "install" # if this fails, try separate make/make install steps
   end
